4f266f09324a216b3f80756d853c35d0c9882153
Ahmed Fadlu <ahmedfadlu28@gmail.com>
2024-12-17 22:36:04 +0700

    Add baru

---
+++ app/src/main/java/id/ac/polbeng/ahmedfadlu/githubprofile/AppGlideModule.kt
@@ -0,0 +1,6 @@
+package id.ac.polbeng.ahmedfadlu.githubprofile
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+@GlideModule
+class AppGlideModule : AppGlideModule()
